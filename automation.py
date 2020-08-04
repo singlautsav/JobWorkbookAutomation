6f998ece7ea43a2c10c73918eebbf2925df97a65
Utsav Singla <utsav18321@iiitd.ac.in>
2020-08-04 12:44:37 +0530

    hello world

--- automation.py
+++ automation.py
@@ -9,8 +9,35 @@ today = date.today()
 
 # conn = sqlite3.connect('cciAutomation.db')
 # c = conn.cursor()
+
 # def updateOldTable():
+
+def updateRecord(val):
+    stringX = "Completed"
+    q = f'''UPDATE jobTask SET workDetail = "Completed" WHERE jobNumber = {val}'''
+    conn = sqlite3.connect('cciAutomation.db')
+    c = conn.cursor()
+    x = c.execute(q)
+    print(f"Updated Record number {val} to completed")
+    conn.commit()
+
+
+
+def deleteARecord(val):
+    q= f''' DELETE FROM jobTask where jobNumber ={val}'''
+    conn = sqlite3.connect('cciAutomation.db')
+    c = conn.cursor()
+    x = c.execute(q)
+    conn.commit()
+
+
+
+
+
 def getOldTable(val):
+    updateRecordX = input("Do You want to update this record to complete (Y/N): ")
+    if updateRecordX=='Y' or updateRecordX=='y':
+        updateRecord(val)
     q = f'''SELECT * FROM jobTask j where j.jobNumber={val}'''
     conn = sqlite3.connect('cciAutomation.db')
     c = conn.cursor()
@@ -33,6 +60,9 @@ def getOldTable(val):
     sheet['C14'] = valX[8]
     sheet['C15'] = valX[9]
     sheet['B16'] = f"Minimum charges for the repair are: {valX[10]}"
+    sheet['E10'] = valX[10]
+    sheet['F11'] = valX[11]
+    sheet['F10'] = valX[12]
 
 
 def createNewTable():
@@ -67,9 +97,13 @@ def createNewTable():
     AdditionalComments = input("Additional Comments: ")
     sheet['C15'] = AdditionalComments
     MinimumCharges = input("Minimum Charges: ")
+    sheet['E10'] = MinimumCharges
     sheet['B16'] = f"Minimum charges for the repair are: {MinimumCharges}"
-    q = f'''INSERT INTO jobTask (jobNumber, orderDate, clientName, clientNumber, clientEmail, itemRecieved, modelNumber, ProblemReported, ProblemDiagnosed, AdditionalComments,minimumCharges) VALUES (?,?,?,?,?,?,?,?,?,?,?)'''
-    vals = (orderNumber,orderDate,ClientName,ClientPhone,ClientEmail,ItemRcvd,ModelNumber,ProblemReported,ProblemDiagnosed,AdditionalComments,int(MinimumCharges))
+    sheet['F11'] = "Pending"
+    advanceRecieved = int(input("Advance Recieved: "))
+    sheet['F10'] = advanceRecieved
+    q = f'''INSERT INTO jobTask (jobNumber, orderDate, clientName, clientNumber, clientEmail, itemRecieved, modelNumber, ProblemReported, ProblemDiagnosed, AdditionalComments,minimumCharges,workDetail,advanceRecieved) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?1)'''
+    vals = (orderNumber,orderDate,ClientName,ClientPhone,ClientEmail,ItemRcvd,ModelNumber,ProblemReported,ProblemDiagnosed,AdditionalComments,int(MinimumCharges),"Pending",advanceRecieved)
     x = c.execute(q,vals)
     conn.commit()
 
@@ -84,13 +118,20 @@ sheet = rb.active
 # wb_sheet = wb.get_sheet(0)
 
 queryA = input('''Enter 1 for new file Query
-Enter 2 for old file Query: ''')
+Enter 2 for old file Query
+Enter 3 for Deleting Record: ''')
 
 if queryA=='1':
     createNewTable()
+    rb.save("newBill.xlsx")
+    subprocess.Popen("newBill.xlsx",shell=True)
 elif queryA=='2':
     a = int(input("Enter jobNumber: "))
     getOldTable(a)
+    rb.save("newBill.xlsx")
+    subprocess.Popen("newBill.xlsx",shell=True)
+elif queryA=='3':
+    a = int(input("Enter jobNumber: "))
+    deleteARecord(a)
+    print("record Deleted")
 
-rb.save("newBill.xlsx")
-subprocess.Popen("newBill.xlsx",shell=True)
